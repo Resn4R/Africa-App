a0bb7ce7a48968c88fd502054cee28c42ddc01bd
Resn4R <purview-joust.0x@icloud.com>
2024-01-14 15:00:43 +0000

    Initial Commit

---
+++ Africa App/Africa_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Africa_AppApp.swift
+//  Africa App
+//
+//  Created by Vito Borghi on 14/01/2024.
+//
+
+import SwiftUI
+
+@main
+struct Africa_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
