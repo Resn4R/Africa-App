4b4e047f29e0cabecb0da38049331f33bec667b2
Resn4R <purview-joust.0x@icloud.com>
2024-01-15 15:51:13 +0000

    created a bunch of views, tabItems, and added more resources (JSONs)

---
+++ Africa App/App/GalleryView.swift
@@ -0,0 +1,18 @@
+//
+//  GalleryView.swift
+//  Africa App
+//
+//  Created by Vito Borghi on 15/01/2024.
+//
+
+import SwiftUI
+
+struct GalleryView: View {
+    var body: some View {
+        Text("Galleryn")
+    }
+}
+
+#Preview {
+    GalleryView()
+}
---
+++ Africa App/App/VideoListView.swift
@@ -0,0 +1,18 @@
+//
+//  VideoListView.swift
+//  Africa App
+//
+//  Created by Vito Borghi on 15/01/2024.
+//
+
+import SwiftUI
+
+struct VideoListView: View {
+    var body: some View {
+        Text("Video")
+    }
+}
+
+#Preview {
+    VideoListView()
+}
