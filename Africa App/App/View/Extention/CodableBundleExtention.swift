504fff18d87c2b56a86467875909ccbb7752572b
Resn4R <purview-joust.0x@icloud.com>
2024-01-21 16:43:43 +0000

    created codable bundle extension

---
+++ Africa App/App/View/Extention/CodableBundleExtention.swift
@@ -0,0 +1,27 @@
+//
+//  CodableBundleExtention.swift
+//  Africa App
+//
+//  Created by Vito Borghi on 21/01/2024.
+//
+
+import Foundation
+
+extension Bundle {
+    func decode(_ file: String) -> [CoverImage] {
+        //locate file
+        guard let url = self.url(forResource: file, withExtension: nil) else { fatalError("Failed to locate \(file) in bundle.") }
+        
+        //create property for data
+        guard let data = try? Data(contentsOf: url) else { fatalError("Failed to load \(file) from bundle.") }
+        
+        //create decoder
+        let decoder = JSONDecoder()
+        
+        //create property for the decoded data
+        guard let loaded = try? decoder.decode([CoverImage].self, from: data) else { fatalError("Failed to decoded \(file) from bundle.") }
+        
+        //return ready data
+        return loaded
+    }
+}
