a743af6308d3f67dd75087ec0f2f6cccfdd846ae
Vito <purview-joust.0x@icloud.com>
2024-01-30 21:13:39 +0000

    finished overlay on mapview

--- Africa App/App/MApView.swift
+++ Africa App/App/MApView.swift
@@ -22,6 +22,17 @@ struct MapView: View {
         return cameraPos
     }()
     
+    @State private var region: MKCoordinateRegion = {
+        
+        let mapCoordinates = CLLocationCoordinate2D(latitude: 6.600286, longitude: 16.4377599)
+        let mapZoomLevel = MKCoordinateSpan(latitudeDelta: 70, longitudeDelta: 70)
+        
+        let mapRegion = MKCoordinateRegion(center: mapCoordinates, span: mapZoomLevel)
+                
+        return mapRegion
+    }()
+    
+    
     let locations: [NationalParkLocation] = Bundle.main.decode("locations.json")
     
     var body: some View {
@@ -32,12 +43,39 @@ struct MapView: View {
                 }
             }
         }
-        .overlay {
+        .overlay (
             HStack (alignment: .center, spacing: 12) {
                 Image("compass")
                     .resizable()
                     .scaledToFit()
                     .frame(width: 48, height: 48)
+                
+                VStack(alignment: .leading, spacing: 3) {
+                    HStack {
+                        Text("Latitude: ")
+                            .font(.footnote)
+                            .bold()
+                            .foregroundStyle(.accent)
+                        
+                        Spacer()
+                        
+                        Text("\(region.center.latitude)")
+                            .font(.footnote)
+                            .foregroundStyle(.white)
+                    }
+                    HStack {
+                        Text("Longitude: ")
+                            .font(.footnote)
+                            .bold()
+                            .foregroundStyle(.accent)
+                        
+                        Spacer()
+                        
+                        Text("\(region.center.longitude)")
+                            .font(.footnote)
+                            .foregroundStyle(.white)
+                    }
+                }
             }
             .padding(.vertical, 12)
             .padding(.horizontal, 16)
@@ -46,7 +84,9 @@ struct MapView: View {
                     .clipShape(RoundedRectangle(cornerRadius: 8))
                     .opacity(0.6)
             )
-        }
+            .padding()
+            , alignment: .top
+        )
     }
 }
 
