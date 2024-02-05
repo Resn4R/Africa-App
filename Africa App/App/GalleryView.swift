6c69984b7462ff149aa6be561ef9422d152c55d4
Vito <purview-joust.0x@icloud.com>
2024-02-05 13:06:09 +0100

    implemented gallery view(Vgrid and hero image)

--- Africa App/App/GalleryView.swift
+++ Africa App/App/GalleryView.swift
@@ -8,13 +8,38 @@
 import SwiftUI
 
 struct GalleryView: View {
+    @State private var selectedAnimal: String = "lion"
+    
+    let gridLayout = Array(repeating: GridItem(.flexible()), count: 3)
+    
+    let animals: [Animal] = Bundle.main.decode("animals.json")
+    
     var body: some View {
         ScrollView(.vertical, showsIndicators: false){
-            Text("Gallery")
+            VStack(alignment: .center, spacing: 30) {
+                
+                Image(selectedAnimal)
+                    .resizable()
+                    .scaledToFit()
+                    .clipShape(Circle())
+                    .overlay(Circle().stroke(Color.white, lineWidth: 8))
+                
+                LazyVGrid(columns: gridLayout, alignment: .center, spacing: 10) {
+                    ForEach(animals) { animal in
+                        Image(animal.image)
+                            .resizable()
+                            .scaledToFit()
+                            .clipShape(Circle())
+                            .overlay(Circle().stroke(Color.white, lineWidth: 1))
+                            .onTapGesture {
+                                selectedAnimal = animal.image
+                            }
+                    }
+                }
+            }
         }
         .frame(maxWidth: .infinity, maxHeight: .infinity)
         .background(MotionAnimationView())
-        
     }
 }
 
